19dc963f34ad3d99c93484f4048f14c2d3807719
ali <abadakhshan@gmail.com>
2021-11-05 20:29:21 +0330

    fix range types

--- projects/narik-custom-validators/src/lib/range/directive.ts
+++ projects/narik-custom-validators/src/lib/range/directive.ts
@@ -1,44 +1,56 @@
-import { Directive, Input, forwardRef, OnInit, OnChanges, SimpleChanges } from '@angular/core';
-import { NG_VALIDATORS, Validator, ValidatorFn, AbstractControl } from '@angular/forms';
+import {
+    Directive,
+    Input,
+    forwardRef,
+    OnInit,
+    OnChanges,
+    SimpleChanges,
+} from '@angular/core';
+import {
+    NG_VALIDATORS,
+    Validator,
+    ValidatorFn,
+    AbstractControl,
+} from '@angular/forms';
 
 import { range } from './validator';
 
 const RANGE_VALIDATOR: any = {
-  provide: NG_VALIDATORS,
-  useExisting: forwardRef(() => RangeValidator),
-  multi: true
+    provide: NG_VALIDATORS,
+    useExisting: forwardRef(() => RangeValidator),
+    multi: true,
 };
 
 @Directive({
-  selector: '[range][formControlName],[range][formControl],[range][ngModel]',
-  providers: [RANGE_VALIDATOR]
+    selector: '[range][formControlName],[range][formControl],[range][ngModel]',
+    providers: [RANGE_VALIDATOR],
 })
 export class RangeValidator implements Validator, OnInit, OnChanges {
-  @Input() range: [number];
+    @Input() range: [number, number];
 
-  private validator: ValidatorFn;
-  private onChange: () => void;
+    private validator: ValidatorFn;
+    private onChange: () => void;
 
-  ngOnInit() {
-    this.validator = range(this.range);
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    for (const key in changes) {
-      if (key === 'range') {
-        this.validator = range(changes[key].currentValue);
-        if (this.onChange) {
-          this.onChange();
-        }
-      }
+    ngOnInit() {
+        this.validator = range(this.range);
     }
-  }
 
-  validate(c: AbstractControl): {[key: string]: any} {
-    return this.validator(c);
-  }
+    ngOnChanges(changes: SimpleChanges) {
+        for (const key in changes) {
+            if (key === 'range') {
+                this.validator = range(changes[key].currentValue);
+                if (this.onChange) {
+                    this.onChange();
+                }
+            }
+        }
+    }
 
-  registerOnValidatorChange(fn: () => void): void {
-    this.onChange = fn;
-  }
+    validate(c: AbstractControl): { [key: string]: any } {
+        return this.validator(c);
+    }
+
+    registerOnValidatorChange(fn: () => void): void {
+        this.onChange = fn;
+    }
 }
